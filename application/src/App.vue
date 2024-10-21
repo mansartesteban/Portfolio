dc9125d062b82e04030dbce738fdd3efa3df5127
Esteban Mansart <esteban.mansart@gmail.com>
2024-10-21 23:43:06 +0200

    Basis of content

--- application/src/App.vue
+++ application/src/App.vue
@@ -1,3 +1,38 @@
 <template>
-    <p>Default project page (with vue)</p>
+    <!-- https://www.codewithrandom.com/2024/05/09/personal-portfolio-website-html-css-js/ -->
+    <div class="main-thread">
+        <aside class="section left-side">
+            <h1>Esteban Mansart</h1>
+            <p>Développeur Web Front-End / Back-End / Week-End</p>
+            <hr>
+            <h2>Expériences</h2>
+            <h2>Formations</h2>
+            <h2>Technologies maîtrisées</h2>
+        </aside>
+        <main class="section right-side">Je suis une section</main>
+
+    </div>
 </template>
+
+<style scoped>
+.main-thread {
+    width: 75%;
+    padding: 1.5rem;
+    display: flex;
+    gap: 2rem;
+    align-items: flex-start;
+}
+
+.left-side {
+    /* width:25%; */
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+    text-align: center;
+    flex: 1;
+}
+
+.right-side {
+    flex: 3;
+}
+</style>
